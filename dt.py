a999095015c3aa639ef0c637662d82dfcf06e48d
huseyin-yildiz <huseyin3421___@hotmail.com>
2023-05-05 00:59:40 +0300

    Decision tree classifier completed. Tests are completed succesfully.

--- dt.py
+++ dt.py
@@ -28,9 +28,11 @@ class Node:
         self.left = None
         self.right = None
         self.classes = np.unique(y)
-        
+        self.class_label = None     # If the leaf node
+
         self.decision_value = None
-        self.data_type = None
+        self.decision_type = None
+        self.decision_attribute = None
 
     # Calculate the entropy of the node with their data
     def calc_entropy(self):
@@ -66,23 +68,23 @@ class Node:
 
     # Generate left and right nodes with given split point
     def generate_nodes(self, split_pt):
-        data_type = self.attribute_types[split_pt[0]]
+        decision_type = self.attribute_types[split_pt[0]]
         decision_value = split_pt[1]
         
         # Numeric
-        if(data_type == 1):
-            mask = x[:,split_pt[0]] < decision_value
+        if(decision_type == 1):
+            mask = self.x[:,split_pt[0]] < decision_value
             
         # Categoric
-        elif(data_type == 2):
-            mask = x[:,split_pt[0]] == decision_value
+        elif(decision_type == 2):
+            mask = self.x[:,split_pt[0]] == decision_value
         
         # Invalid attribute type
         else:
             raise TypeError("Attribute type is not valid. Only 1 and 2 is valid ")
 
-        left = Node(x[mask], y[mask], attribute_types)
-        right = Node(x[~mask], y[~mask], attribute_types)
+        left = Node(self.x[mask], self.y[mask], attribute_types)
+        right = Node(self.x[~mask], self.y[~mask], attribute_types)
 
         return (left, right)
     
@@ -97,24 +99,57 @@ class Node:
         total = left_size + right_size
 
         # Normalization
-        score = left_size / total * left.calc_entropy() + right_size / total * right.calc_entropy()
+        score = left_size / total * node_left.calc_entropy() + right_size / total * node_right.calc_entropy()
 
         return score
 
 
+    # Predict the given data
+    # Go until leaf node recursively and return class label
+    def predict(self, data):
+        
+        if(self.class_label != None):
+            return self.class_label
+
+        # Numeric
+        if(self.decision_type == 1):
+           if( data[self.decision_attribute] < self.decision_value):
+               return self.left.predict(data)
+           
+           else:
+               return self.right.predict(data)    
+        
+        # Categoric
+        elif(self.decision_type == 2):
+            if( data[self.decision_attribute] == self.decision_value):
+               return self.left.predict(data)
+           
+            else:
+                return self.right.predict(data)    
+        
+
+        # Invalid attribute type
+        else:
+            raise TypeError("Attribute type is not valid. Only 1 and 2 is valid ")
 
 
 
 # Recursive and entropy based DT generation algorithm  by using greedy algorithm
+# Take data included root node and generate the DT recursively
 def generate_tree(node:Node , max_depth):
 
-    # If max depth is reached, then terminates
+    # If max depth is reached, then label the leaf node and terminates
     if(max_depth <= 0):
-        return None
+        counts = np.bincount(node.y)
+        most_freq = np.argmax(counts)
+        node.class_label = node.y[most_freq]
+        return
     
     # If the entropy of the current node is 0 then no need to continue anymore
+    # Label the leaf node and return
     if(node.calc_entropy() == 0):
-        return None
+        node.class_label = node.y[0]
+        return
     
     # Find all split points
     split_points = node.find_split_pts()
@@ -139,10 +174,37 @@ def generate_tree(node:Node , max_depth):
             nodes = (node_left, node_right)
 
     
-    # Best split found.
+    # End of For : Best split found.
+
+    # Place the children to the left and right 
+    node.left, node.right = node_left, node_right
+    
+    # Place the decision value and data type on the node
+    node.decision_value = best_split[1]
+    node.decision_type = node.attribute_types[best_split[0]]
+    node.decision_attribute = best_split[0]
+
+    # Recursive call for children
+    generate_tree(node.left, max_depth-1)
+    generate_tree(node.right, max_depth-1)
+    
 
 
 
+# DT builder
+def buid_dt(X, y, attribute_types, max_depth):
+    root = Node(X,y,attribute_types)
+    generate_tree(root, max_depth)
+    return root
+
+
+
+
+
+# Takes DT and X matrix returns a vector for predicted predicted labels
+def predict_dt(dt:Node, X):
+    predict_vector = [dt.predict(x) for x in X]
+    return np.array( predict_vector )
 
 
 
@@ -154,39 +216,98 @@ def generate_tree(node:Node , max_depth):
 import pandas as pd
 import numpy as np
 
-# df = pd.read_csv("trial.csv")
+
+# # create a pandas DataFrame
+# df = pd.DataFrame({
+#     'Gender': ['Male', 'Female', 'Male', 'Female', 'Male'],
+#     'Age': [25, 42, 32, 18, 47],
+#     'Income': [50000, 60000, 70000, 45000, 80000],
+#     'MaritalStatus': ['Married', 'Single', 'Married', 'Single', 'Married'],
+#     'BoughtInsurance': ['Yes', 'No', 'Yes', 'No', 'Yes']
+# })
+
+# print(df,"\n")
+
+# # Split x and y
+# y = df["BoughtInsurance"].values
+# x = df.drop("BoughtInsurance",axis=1)
+
+# attribute_types = [2,1,1,2]
+# x = x.values
 
 
-# create a pandas DataFrame
-df = pd.DataFrame({
-    'Gender': ['Male', 'Female', 'Male', 'Female', 'Male'],
-    'Age': [25, 42, 32, 18, 47],
-    'Income': [50000, 60000, 70000, 45000, 80000],
-    'MaritalStatus': ['Married', 'Single', 'Married', 'Single', 'Married'],
-    'BoughtInsurance': ['Yes', 'No', 'Yes', 'No', 'Yes']
-})
+# root = Node(x,y,attribute_types)
 
-print(df,"\n")
+# val = root.calc_entropy()
+# print("root entropy:",val,"\n")
+
+
+# split_pts = root.find_split_pts()
+
+# print("Split Points:")
+# for pt in split_pts:
+#     left,right = root.generate_nodes(pt)
+#     split_score = root.calc_decision_score(left,right)
+#     print(pt, "Score:", split_score)
+    
+
+# # generate_tree(root,5)
+
+
+# dt = buid_dt(x,y,attribute_types,5)
+# print("ok")
+
+# for i,data in enumerate(x):
+#     predicted = dt.predict(data)
+#     if(predicted == y[i]):
+#         print("True")
+#     else:
+#         print("False")
+    
+# predicted = predict_dt(dt,x)
+
+# print("Predicted:",predicted)
+# print("Actual   :",y)
+
+
+
+
+
+# Test on huge dataset
+
+
+
+df1 = pd.read_csv("trial.csv")
 
 # Split x and y
-y = df["BoughtInsurance"].values
-x = df.drop("BoughtInsurance",axis=1)
-
-attribute_types = [2,1,1,2]
-x = x.values
+Y = df1["Risk"].values
+X = df1.drop("Risk",axis=1)
+X = df1.drop("LOCATION_ID",axis=1).values
 
 
-root = Node(x,y,attribute_types)
+attribute_types = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
 
-val = root.calc_entropy()
-print("root entropy:",val,"\n")
+from sklearn.model_selection import KFold
+from sklearn.metrics import confusion_matrix, classification_report
+
+k_fold = KFold(n_splits=6, shuffle=True, random_state=42)
+
+print(X[1])
+
+for k, (train, test) in enumerate(k_fold.split(X, Y)):
+  # Train
+  dt = buid_dt(X[train], Y[train],attribute_types,5)
+  y_pred = predict_dt(dt, X[test])
+  print("\n\nFold",k,":")
+  # confusion matrix
+  conf_mat = confusion_matrix(Y[test], y_pred)
+  # Display confusion matrix
+  cm_df = pd.DataFrame(conf_mat, columns=['Predicted 0', 'Predicted 1'], index=['True 0', 'True 1'])
+  print('Confusion matrix:')
+  print(cm_df)
+  print("\nResult:")
+  print( classification_report(Y[test],y_pred) )
+  print()
 
 
-split_pts = root.find_split_pts()
 
-print("Split Points:")
-for pt in split_pts:
-    left,right = root.generate_nodes(pt)
-    split_score = root.calc_decision_score(left,right)
-    print(pt, "Score:", split_score)
-    
